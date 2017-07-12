cff8629721cc5ad44b96c19ea53ab84a84e20ba3
penglibit <peng@bitmotive.com>
2017-07-12 13:05:58 -0400

    Update __init__.py

--- openid/__init__.py
+++ openid/__init__.py
@@ -46,3 +46,14 @@ __all__ = [
     'urinorm',
     'yadis',
 ]
+
+# Parse the version info
+try:
+    version_info = map(int, __version__.split('.'))
+except ValueError:
+    version_info = (None, None, None)
+else:
+    if len(version_info) != 3:
+        version_info = (None, None, None)
+    else:
+        version_info = tuple(version_info)
